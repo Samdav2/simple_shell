3a611436a7da0a42f46f47df98e0b633896b0689
Samdav2 <dawodujohn1@gmail.com>
2023-04-17 18:25:23 +0100

    Simple code that makes betty proud

---
+++ 0-make_betty_proud.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - C program designed just to make betty proud
+ * 
+ * Return: Void
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 5;
+
+	b = 10;
+
+	c = a + b;
+
+	printf("%d\n", c);
+
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Hello, welocme to our simple shell project by ALX.
+This files will list and explain all the function each file in this directory will be performing
